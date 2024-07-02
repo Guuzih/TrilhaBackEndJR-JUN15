4c0665c160d11a4c50ad64cea656a0f726fa49e1
Gustavo Coelho <136639290+Guuzih@users.noreply.github.com>
2024-07-02 15:29:26 -0300

    Add files via upload

--- src/middlewares/rateLimit.js
+++ src/middlewares/rateLimit.js
@@ -1,8 +1,8 @@
 const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutos
-    max: 100, // limite de 100 requisições por IP por janela de tempo
+    windowMs: 15 * 60 * 1000,
+    max: 100,
     message: 'Too many requests from this IP, please try again later'
 });
 
